435e57dddc5e1e29f66d9c2c738c7470e4d63ed8
Martin Schneider <martin.schne@protonmail.com>
2024-09-26 22:38:07 +0200

    add icons and splash screens for ios and android, adjust the app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "ionic.relaxingsounds",
-  appName: "relaxingsounds",
+  appName: "Relaxing Sounds",
   webDir: "dist",
 };
 
